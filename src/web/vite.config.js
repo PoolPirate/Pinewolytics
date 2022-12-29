7985ad9c055b86e47d057c4de901e1504358620f
Playwo <eliaswolf2001@t-online.de>
2022-12-29 13:38:01 +0100

    Add dev proxy

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -1,10 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import * as path from 'path';
+import { defineConfig } from 'vite'
 
-/** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 };
 
-export default config;
+const devConfig = {
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			"/Api": {
+				target: "http://127.0.0.1:4565"
+			}
+		}
+	}
+}
+
+export default defineConfig(({ command, mode }) => {
+	const isDev = mode === 'development';
+
+	return isDev
+		? devConfig
+		: config;
+});
 
