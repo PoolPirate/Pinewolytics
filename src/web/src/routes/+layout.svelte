c5834472fba00a67eb72147f2f434cb09bea552b
Playwo <eliaswolf2001@t-online.de>
2022-12-31 00:50:30 +0100

    Rotation background

--- src/web/src/routes/+layout.svelte
+++ src/web/src/routes/+layout.svelte
@@ -1,7 +1,21 @@
+<script lang="ts" context="module">
+	var rotation: number = 0;
+</script>
+
 <script lang="ts">
 	import '../app.css';
 	import pattern from '$lib/static/pattern.png';
-	import { beforeUpdate } from 'svelte';
+	import { beforeUpdate, onMount } from 'svelte';
+
+	onMount(tickRotation);
+
+	function tickRotation() {
+		const rootElement = document.querySelector(':root')! as any;
+		rootElement.style.setProperty('--rotation', rotation);
+
+		rotation++;
+		setTimeout(tickRotation, 50);
+	}
 
 	beforeUpdate(() => {
 		const rootElement = document.querySelector(':root')! as any;
@@ -22,7 +36,12 @@
 <style>
 	.background {
 		background: #4261cf;
-		background: -moz-linear-gradient(45deg, var(--color1), 0%, var(--color2) 100%);
+		background: -moz-linear-gradient(
+			calc(var(--rotation) * 1deg),
+			var(--color1),
+			0%,
+			var(--color2) 100%
+		);
 		background: -webkit-gradient(
 			linear,
 			left bottom,
@@ -30,13 +49,27 @@
 			color-stop(0%, var(--color1)),
 			color-stop(100%, var(--color2))
 		);
-		background: -webkit-linear-gradient(45deg, var(--color1) 0%, var(--color2) 100%);
-		background: -o-linear-gradient(45deg, var(--color1) 0%, var(--color2) 100%);
-		background: -ms-linear-gradient(45deg, var(--color1) 0%, var(--color2) 100%);
-		background: linear-gradient(45deg, var(--color1) 0%, var(--color2) 100%);
+		background: -webkit-linear-gradient(
+			calc(var(--rotation) * 1deg),
+			var(--color1) 0%,
+			var(--color2) 100%
+		);
+		background: -o-linear-gradient(
+			calc(var(--rotation) * 1deg),
+			var(--color1) 0%,
+			var(--color2) 100%
+		);
+		background: -ms-linear-gradient(
+			calc(var(--rotation) * 1deg),
+			var(--color1) 0%,
+			var(--color2) 100%
+		);
+		background: linear-gradient(calc(var(--rotation) * 1deg), var(--color1) 0%, var(--color2) 100%);
 
 		height: 100vh;
 		width: 100vw;
+
+		animation: rotate 120s infinite;
 	}
 
 	.pattern:before {
