ced9ad44a4c75fa35eb103e9e7ec862b988df84a
Playwo <eliaswolf2001@t-online.de>
2023-01-03 20:06:47 +0100

    Collapsable sidebar

--- src/web/src/routes/(analytics)/AnalyticsLayout.svelte
+++ src/web/src/routes/(analytics)/AnalyticsLayout.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+	import Hamburger from '$lib/components/Hamburger.svelte';
+
+	var open: boolean;
+</script>
+
+<div class="fixed m-4 ontopontop">
+	<Hamburger bind:open />
+</div>
+
+<div class="pointer-events-none fixed w-1/2 lg:w-1/4 h-screen ontop">
+	<div
+		class:open
+		class="pointer-events-auto sidebar ontop w-full h-full px-4 pt-16
+		       [&>aside]:flex [&>aside]:flex-col
+			   [&>*>a]:p-4 [&>*>a]:bg-gray-400 [&>*>a]:m-2"
+	>
+		<slot name="sidebar" />
+	</div>
+</div>
+
 <div class="flex flex-col w-full h-screen">
 	<div class="relative flex flex-row justify-around items-center h-14 navbar px-4 text-white">
 		<slot name="nav" />
@@ -9,6 +30,13 @@
 </div>
 
 <style>
+	.ontop {
+		z-index: 2;
+	}
+	.ontopontop {
+		z-index: 3;
+	}
+
 	.navbar:before {
 		content: ' ';
 		display: block;
@@ -17,5 +45,19 @@
 		width: 100%;
 		height: 100%;
 		opacity: 50%;
+	}
+
+	.sidebar {
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: -100%;
+		background-color: black;
+		overflow-x: hidden;
+		transition: left 0.4s ease-in-out;
+	}
+
+	.open {
+		left: 0;
 	}
 </style>
--- src/web/src/routes/(analytics)/optimism/+layout.svelte
+++ src/web/src/routes/(analytics)/optimism/+layout.svelte
@@ -13,11 +13,16 @@
 </script>
 
 <AnalyticsLayout>
-	<nav slot="nav" class="h-full w-full flex flex-row items-center">
-		<BackToMenuLink />
+	<aside slot="sidebar">
+		<a href="/optimism">Overview</a>
+		<a href="/optimism/transactions">Transactions</a>
+		<a href="/optimism/op-token">$OP Token</a>
+	</aside>
 
+	<nav slot="nav" class="h-full w-full flex flex-row items-center">
 		<h2 class="ml-auto font-bold text-xl">Optimism</h2>
 		<img src={optimismLogo} alt="Optimism" class="ml-6 max-w-full max-h-full p-1" />
 	</nav>
+	<nav />
 	<slot slot="main" />
 </AnalyticsLayout>
