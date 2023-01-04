5e876da4c624fa2c097a77ec780d5eb83c681d21
IPowerW <Leon@wenderoth.de>
2023-01-04 18:16:19 +0100

    Gauge

---
+++ src/web/src/lib/charts/SingleValueChart.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Chart from '$lib/components/Chart.svelte';
+	import type {
+		EChartsOption,
+		SeriesOption,
+		YAXisComponentOption,
+		LegendComponentOption
+	} from 'echarts';
+	import jsonLogo from '$lib/static/logo/json.svg';
+
+	export let series: SeriesOption[];
+	export let yAxis: YAXisComponentOption = {
+		type: 'value'
+	};
+	export let legend: LegendComponentOption = {};
+
+	var options: EChartsOption;
+
+	$: makeOptions(series);
+
+	function makeOptions(series: SeriesOption[]) {
+		options = {
+			legend: legend,
+			toolbox: {
+				itemSize: 40,
+				left: 'right',
+			},
+			series: series
+		};
+	}
+</script>
+
+<Chart isLoading={false} {options} />
--- src/web/src/routes/(analytics)/terra/transactions/fees/+page.svelte
+++ src/web/src/routes/(analytics)/terra/transactions/fees/+page.svelte
@@ -1,44 +1,129 @@
 <script lang="ts">
-	import { QueryName, QuerySubscriptionBuilder } from '$lib/service/querysubscription';
+	import {createQueryListener, QueryName, QuerySubscriptionBuilder} from '$lib/service/querysubscription';
 	import type { LegendComponentOption, SeriesOption, YAXisComponentOption } from 'echarts';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 
 	import TimeSeriesChartRoundLog from '$lib/charts/TimeSeriesChart.svelte';
 	import type {TerraTotalFeeDTO, TerraTransactionMetricsDTO} from '$lib/models/DTOs/TerraDTOs';
+	import SingleValueChart from "$lib/charts/SingleValueChart.svelte";
 
-	const valuesStoreTransactionMetric = writable<TerraTransactionMetricsDTO[]>([]);
-	const valuesStoreTotalFeesHistory = writable<TerraTotalFeeDTO[]>([]);
+	const subscriptionBuilder = new QuerySubscriptionBuilder()
+	const valuesStoreTransactionMetric = createQueryListener(subscriptionBuilder,QueryName.TerraTransactionMetricsHistory)
+	const valuesStoreTotalFeesHistory = createQueryListener(subscriptionBuilder,QueryName.TerraTotalFeesHistory)
 	const seriesFeeTotal = writable<SeriesOption[]>([]);
 	const seriesFee = writable<SeriesOption[]>([]);
 
 	onMount(async () => {
-		await new QuerySubscriptionBuilder()
-			.addQuery(QueryName.TerraTransactionMetricsHistory, (value) => {
-				valuesStoreTransactionMetric.set(value);
-			})
-			.addQuery(QueryName.TerraTotalFeesHistory, (value) => {
-				valuesStoreTotalFeesHistory.set(value);
-			})
-			.start();
+		await subscriptionBuilder.start();
 	});
 
 	$: makeSeriesFeeTotal($valuesStoreTotalFeesHistory);
 
+	function helpme(a,b) {
+		return a-b;
+    }
+
 	function makeSeriesFeeTotal(values: TerraTotalFeeDTO[]) {
+		if(values.length == 0)
+            return
+
+		const sortValues = values.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
+
+		var last30 =  helpme(sortValues[.0].feesSinceInception,sortValues[30].feesSinceInception)
+		var last60 =  helpme(sortValues[30].feesSinceInception,sortValues[60].feesSinceInception)
+		var last90 =  helpme(sortValues[90].feesSinceInception,sortValues[120].feesSinceInception)
+
+        const lastmax = Math.max(last90,last60,last30)
+
+		 last30 /= lastmax
+		 last60 /=  lastmax
+		 last90 /=  lastmax
+
+
+		const gaugeData=[
+			{
+				value: Math.ceil(last30*100),
+				name: 'last30',
+				title: {
+					offsetCenter: ['0%', '-30%']
+				},
+				detail: {
+					valueAnimation: true,
+					offsetCenter: ['0%', '-20%']
+				}
+			},
+			{
+				value: Math.ceil(last60*100),
+				name: 'last60',
+				title: {
+					offsetCenter: ['0%', '0%']
+				},
+				detail: {
+					valueAnimation: true,
+					offsetCenter: ['0%', '10%']
+				}
+			},
+			{
+				value: Math.ceil(last90*100),
+				name: 'last90',
+				title: {
+					offsetCenter: ['0%', '30%']
+				},
+				detail: {
+					valueAnimation: true,
+					offsetCenter: ['0%', '40%']
+				}
+			}
+		]
+
 
 		seriesFeeTotal.set([
 			{
-				type: 'bar',
-				data: values
-					.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
-					.map((x) => [new Date(x.timestamp).getTime(), x.feesSinceInception]),
+				type: 'gauge',
+				startAngle: 90,
+				endAngle: -270,
+				pointer: {
+					show: false
+				},
+				progress: {
+					show: true,
+					overlap: false,
+					roundCap: true,
+					clip: false,
 
-				showBackground: true,
-				backgroundStyle: {
-					color: 'rgba(180, 180, 180, 0.2)'
+				},
+				axisLine: {
+					lineStyle: {
+						width: 40
+					}
+				},
+				splitLine: {
+					show: false,
+					distance: 0,
+					length: 10
+				},
+				axisTick: {
+					show: false
+				},
+				axisLabel: {
+					show: false,
+				},
+				data: gaugeData,
+				title: {
+					fontSize: 14,
+				},
+				detail: {
+					width: 50,
+					height: 14,
+					fontSize: 14,
+					color: 'inherit',
+					borderColor: 'inherit',
+					borderRadius: 20,
+					borderWidth: 1,
+					formatter: '{value}%'
 				}
-			},
+				},
 		]);}
 
 
@@ -125,4 +210,4 @@
 </script>
 
 <TimeSeriesChartRoundLog {yAxis} {legend} series={$seriesFee} />
-<TimeSeriesChartRoundLog {yAxis} {legend} series={$seriesFeeTotal} />
+<SingleValueChart {yAxis} {legend} series={$seriesFeeTotal} />
