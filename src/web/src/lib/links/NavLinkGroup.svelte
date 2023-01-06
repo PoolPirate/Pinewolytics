258d6dab16eb67d3f29a72f3ffb03344ed38531a
Playwo <eliaswolf2001@t-online.de>
2023-01-06 19:55:51 +0100

    Add direction indicator

--- src/web/src/lib/links/NavLinkGroup.svelte
+++ src/web/src/lib/links/NavLinkGroup.svelte
@@ -6,13 +6,19 @@
 	var contentElement: HTMLElement;
 </script>
 
-<button on:click={() => (expanded = !expanded)} class="p-2 m-2 rounded-lg hover:font-bold">
-	{title}
+<button
+	on:click={() => (expanded = !expanded)}
+	class="flex flex-row items-center p-2 m-2 rounded-lg hover:font-bold"
+>
+	<p class="text-xl mr-4 transition-all duration-300" class:rotate-180={expanded}>▲</p>
+	<p>
+		{title}
+	</p>
 </button>
 
 <div
 	bind:this={contentElement}
-	class="grid ml-3 max-h-0 overflow-hidden"
+	class="grid ml-3 max-h-0 overflow-hidden transition-all duration-300"
 	style="max-height: {expanded ? contentElement.scrollHeight : 0}px;"
 >
 	<slot />
@@ -33,9 +39,5 @@
 		background: linear-gradient(0, var(--color1), var(--color2));
 		opacity: 80%;
 		border-radius: inherit;
-	}
-
-	div {
-		transition: max-height 0.2s ease-in-out;
 	}
 </style>
