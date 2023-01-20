fea4efc19503051ab02f536a0d606d4d9082f58f
Playwo <eliaswolf2001@t-online.de>
2023-01-20 18:39:24 +0100

    Add source code

--- src/web/src/routes/(tools)/osmosis/ranking/info/+page.svelte
+++ src/web/src/routes/(tools)/osmosis/ranking/info/+page.svelte
@@ -2,6 +2,7 @@
 	import RankingLayout from '../RankingLayout.svelte';
 	import InfoLogo from '$lib/static/logo/info.svg';
 	import FlipsideLogo from '$lib/static/logo/flipside.png';
+	import GithubLogo from '$lib/static/logo/github.png';
 </script>
 
 <RankingLayout>
@@ -14,10 +15,20 @@
 			href="https://flipsidecrypto.xyz/"
 			target="_blank"
 			rel="noreferrer"
-			class="flex flex-row items-center gap-4 border rounded-lg p-2 border-gray-700 border-2 bg-gray-400"
+			class="flex flex-row justify-between items-center gap-4 border rounded-lg p-2 border-gray-700 border-2 bg-gray-400 w-full"
 		>
 			<p class="text-xl">Data Provided By FlipsideCrypto</p>
 			<img alt="FlipsideCrypto Logo" class="h-16" src={FlipsideLogo} />
+		</a>
+
+		<a
+			href="https://github.com/PoolPirate/Pinewolytics"
+			target="_blank"
+			rel="noreferrer"
+			class="flex flex-row justify-between items-center gap-4 border rounded-lg p-2 border-gray-700 border-2 bg-gray-400 w-full"
+		>
+			<p class="text-xl">Source Code</p>
+			<img alt="FlipsideCrypto Logo" class="h-16" src={GithubLogo} />
 		</a>
 
 		<div class="flex flex-row items-center w-full justify-end font-bold ">
