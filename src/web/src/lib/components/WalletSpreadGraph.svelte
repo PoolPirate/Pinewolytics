98861927d254f35698570c82196cac5efe278b22
Playwo <eliaswolf2001@t-online.de>
2023-02-06 20:08:47 +0100

    Add error state to wallet graph page

--- src/web/src/lib/components/WalletSpreadGraph.svelte
+++ src/web/src/lib/components/WalletSpreadGraph.svelte
@@ -11,6 +11,8 @@
 	import WalletGraph, { type DeveloperWallet } from './WalletGraph.svelte';
 	import { browser } from '$app/environment';
 	import type { OsmosisNetTransferDTO } from '$lib/models/DTOs/OsmosisDTOs';
+	import Warning from '$lib/static/warning.svg';
+
 	export let maxDepth: number;
 	export let initialWallets: InitialWallet[] | null;
 
@@ -22,60 +24,77 @@
 	const transfers = writable<OsmosisNetTransferDTO[]>([]);
 
 	const isLoading = writable<boolean>(true);
+	const isFailed = writable<boolean>(false);
 
 	$: startTracing(initialWallets);
 
 	async function startTracing(initialWallets: InitialWallet[] | null) {
-		if (initialWallets == null || !browser) {
-			return;
-		}
-
-		const initialAddresses = initialWallets.map((x) => x.address);
-
-		const totalAddresses = [...initialAddresses];
-
-		wallets.set(
-			initialWallets.map((wallet) => {
-				return {
-					address: wallet.address,
-					level: 0,
-					amount: wallet.amount == null ? 0 : wallet.amount
-				};
-			})
-		);
-
-		for (let i = 1; i < maxDepth; i++) {
-			const relatedAddresses = await getRelatedWallets(totalAddresses);
-
-			if (relatedAddresses.length == 0) {
-				break;
+		try {
+			if (initialWallets == null || !browser) {
+				return;
 			}
 
-			const devWallets = $wallets;
+			const initialAddresses = initialWallets.map((x) => x.address);
 
-			relatedAddresses.forEach((address) => {
-				totalAddresses.push(address);
+			const totalAddresses = [...initialAddresses];
 
-				devWallets.push({
-					address: address,
-					level: i,
-					amount: 0
+			wallets.set(
+				initialWallets.map((wallet) => {
+					return {
+						address: wallet.address,
+						level: 0,
+						amount: wallet.amount == null ? 0 : wallet.amount
+					};
+				})
+			);
+
+			for (let i = 1; i < maxDepth; i++) {
+				const relatedAddresses = await getRelatedWallets(totalAddresses);
+
+				if (relatedAddresses.length == 0) {
+					break;
+				}
+
+				const devWallets = $wallets;
+
+				relatedAddresses.forEach((address) => {
+					totalAddresses.push(address);
+
+					devWallets.push({
+						address: address,
+						level: i,
+						amount: 0
+					});
 				});
-			});
 
-			depth.set(i);
-			wallets.set(devWallets);
+				depth.set(i);
+				wallets.set(devWallets);
+			}
+
+			transfers.set(await getInternalNetOSMOTransfers(totalAddresses));
+			isLoading.set(false);
+		} catch (error) {
+			isFailed.set(true);
 		}
-
-		transfers.set(await getInternalNetOSMOTransfers(totalAddresses));
-		isLoading.set(false);
 	}
 </script>
 
-<WalletGraph
-	class={clazz}
-	wallets={$wallets}
-	transfers={$transfers}
-	depth={$depth + 1}
-	isLoading={$isLoading}
-/>
+{#if !$isFailed}
+	<WalletGraph
+		class={clazz}
+		wallets={$wallets}
+		transfers={$transfers}
+		depth={$depth + 1}
+		isLoading={$isLoading}
+	/>
+{:else}
+	<div>
+		<div
+			class="text-center transparent-background grid grid-cols-1 place-items-center p-5 rounded-xl place-content-start"
+		>
+			<img class="w-64 p-8" src={Warning} alt="Loading" />
+			<h1 class="font-bold text-xl">FlipsideCrypto API Unavailable</h1>
+			<h2>Try again later :/</h2>
+		</div>
+	</div>
+{/if}
--- src/web/src/routes/(analytics)/osmosis/dev-wallets/wallets/+page.svelte
+++ src/web/src/routes/(analytics)/osmosis/dev-wallets/wallets/+page.svelte
@@ -3,23 +3,38 @@
 	import { getDeveloperMintReceivers, getTotalDeveloperMintedOsmo } from '$lib/service/osmosislcd';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
+	import Warning from '$lib/static/warning.svg';
 
 	const initialWallets = writable<InitialWallet[] | null>(null);
+	const loadingFailure = writable<boolean>(false);
 
 	onMount(async () => {
-		const totalOMSO = await getTotalDeveloperMintedOsmo();
-		const devMintReceivers = await getDeveloperMintReceivers();
-		const wallets = devMintReceivers.map((x) => {
-			return {
-				address: x.address,
-				amount: totalOMSO * x.weight
-			};
-		});
-
-		initialWallets.set(wallets);
+		try {
+			const totalOMSO = await getTotalDeveloperMintedOsmo();
+			const devMintReceivers = await getDeveloperMintReceivers();
+			const wallets = devMintReceivers.map((x) => {
+				return {
+					address: x.address,
+					amount: totalOMSO * x.weight
+				};
+			});
+			initialWallets.set(wallets);
+		} catch (error) {
+			loadingFailure.set(true);
+		}
 	});
 </script>
 
-<div class="grid grid-cols-1 w-full h-full">
-	<WalletSpreadGraph class="h-full" maxDepth={10} initialWallets={$initialWallets} />
-</div>
+{#if !$loadingFailure}
+	<div class="grid grid-cols-1 w-full h-full">
+		<WalletSpreadGraph class="h-full" maxDepth={10} initialWallets={$initialWallets} />
+	</div>
+{:else}
+	<div
+		class="text-center transparent-background grid grid-cols-1 place-items-center p-5 rounded-xl"
+	>
+		<img class="w-64 p-8" src={Warning} alt="Loading" />
+		<h1 class="font-bold text-xl">FlipsideCrypto API Unavailable</h1>
+		<h2>Try again later :/</h2>
+	</div>
+{/if}
