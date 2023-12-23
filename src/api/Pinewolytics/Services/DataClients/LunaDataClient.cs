90c3c2e2c660b21f9df0c3b425dce2905eedcbc6
Playwo <eliaswolf2001@t-online.de>
2023-12-23 16:02:18 +0100

    feat: Disable Luna data client

--- src/api/Pinewolytics/Services/DataClients/LunaDataClient.cs
+++ src/api/Pinewolytics/Services/DataClients/LunaDataClient.cs
@@ -10,35 +10,35 @@ public class LunaDataClient : BaseDataClient
     [Inject]
     private readonly CoinGeckoClient CoinGeckoClient = null!;
 
-    [RealtimeValue("Luna-Block-Height", 2 * SECONDS)]
-    public async Task<ulong> LoadPeakBlockHeightAsync()
-    {
-        var (height, _) = await LunaLCDClient.GetLatestBlockInfoAsync();
-        return height;
-    }
+    //[RealtimeValue("Luna-Block-Height", 2 * SECONDS)]
+    //public async Task<ulong> LoadPeakBlockHeightAsync()
+    //{
+    //    var (height, _) = await LunaLCDClient.GetLatestBlockInfoAsync();
+    //    return height;
+    //}
 
-    [RealtimeValue("Luna-Block-Timestamp", 2 * SECONDS)]
-    public async Task<DateTimeOffset> LoadPeakBlockTimestampAsync()
-    {
-        var (_, timestamp) = await LunaLCDClient.GetLatestBlockInfoAsync();
-        return timestamp;
-    }
+    //[RealtimeValue("Luna-Block-Timestamp", 2 * SECONDS)]
+    //public async Task<DateTimeOffset> LoadPeakBlockTimestampAsync()
+    //{
+    //    var (_, timestamp) = await LunaLCDClient.GetLatestBlockInfoAsync();
+    //    return timestamp;
+    //}
 
-    [RealtimeValue("Luna-Price", 10 * SECONDS)]
-    public async Task<double> LoadPriceAsync()
-    {
-        return await CoinGeckoClient.GetLunaPriceAsync();
-    }
+    //[RealtimeValue("Luna-Price", 10 * SECONDS)]
+    //public async Task<double> LoadPriceAsync()
+    //{
+    //    return await CoinGeckoClient.GetLunaPriceAsync();
+    //}
 
-    [RealtimeValue("Luna-Total-Supply", 30 * SECONDS)]
-    public async Task<double> LoadTotalSupplyAsync()
-    {
-        return await LunaLCDClient.GetTotalSupplyAsync();
-    }
+    //[RealtimeValue("Luna-Total-Supply", 30 * SECONDS)]
+    //public async Task<double> LoadTotalSupplyAsync()
+    //{
+    //    return await LunaLCDClient.GetTotalSupplyAsync();
+    //}
 
-    [RealtimeValue("Luna-Circulating-Supply", 30 * SECONDS)]
-    public async Task<double> LoadCirculatingSupplyAsync()
-    {
-        return await LunaLCDClient.GetCirculatingSupplyAsync();
-    }
+    //[RealtimeValue("Luna-Circulating-Supply", 30 * SECONDS)]
+    //public async Task<double> LoadCirculatingSupplyAsync()
+    //{
+    //    return await LunaLCDClient.GetCirculatingSupplyAsync();
+    //}
 }
